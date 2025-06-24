6c178b04e80ea4896cb661c06b2b5d273e03f802
anemochore <fallensky@naver.com>
2025-06-24 10:42:52 +0900

    개정판 정보 입력 로직 수정

--- fillForms.js
+++ fillForms.js
@@ -1,7 +1,7 @@
 //changelog는 깃허브에: https://github.com/anemochore/fill-NL-forms/commits/main
 
 async function fillForms(obj) {
-  obj['정가'] = parseInt(obj['정가'].replace(/^₩/, '').replaceAll(/,/g, ''));
+  obj['정가2'] = parseInt(obj['정가'].replace(/^₩/, '').replaceAll(/,/g, ''));
 
   //서명식별번호
   $('#eaFourthIsbn').val(obj['서명식별번호']).attr("readOnly", false);
@@ -14,7 +14,7 @@ async function fillForms(obj) {
   $('#title')             .val(obj['도서명']);
   $('#subtitle')          .val(obj['부제명']);
   $('[name="originalTitle"]').val(obj['번역서의_원제목']);
-  $('#priceAmount')       .val(obj['정가']);
+  $('#priceAmount')       .val(obj['정가2']);
   $('#width')             .val(obj['가로']);
   $('#height')            .val(obj['세로']);
   $('#page')              .val(obj['페이지수']);
@@ -29,11 +29,12 @@ async function fillForms(obj) {
   document.querySelector('#agreeEssential').click();
 
 
-  //필요시 판수를 개정판으로
-  if(obj['판수'] > 1) {
+  //초판이 아니라면 판 유형 바꾸고 판차 등을 입력
+  if(obj['판유형'] && obj['판유형'] != '신판(초판)') {
     $('#editionType').val('REV');
-    $('#editionNumber').val(obj['판차']);
-    $('#prevProductIDValue').val(obj['전판ISBN']);
+    $('#editionType_REV_LI')[0].classList.remove('blind');
+    $('#editionStatement').val(obj['판차']);
+    $('#prevProductIdValue').val(obj['전판ISBN']);
   }
 
   //저자 + 역자 추가 (삽화가 등은 어쩌지-_-?)
